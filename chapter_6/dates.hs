2b6ffd1a4502fcbfb84602f8f8680d7763369526
Timofey Tipishev <tipishev@gmail.com>
2018-04-08 16:47:04 +0200

    refresh chapter 6 examples

---
+++ chapter_6/dates.hs
@@ -0,0 +1,23 @@
+module Dates where
+
+data DayOfWeek = Mon | Tue | Wed | Thu | Fri | Sat | Sun deriving Show
+
+-- Date acts as both Type- and Value- constructor
+data Date =
+  Date DayOfWeek Int deriving Show -- Constructor
+
+instance Eq DayOfWeek where
+  (==) Mon Mon = True
+  (==) Tue Tue = True
+  (==) Wed Wed = True
+  (==) Thu Thu = True
+  (==) Fri Fri = True
+  (==) Sat Sat = True
+  (==) Sun Sun = True
+  (==) _ _     = False
+
+
+instance Eq Date where
+  (==) (Date weekday dayOfMonth)
+       (Date otherWeekday otherDayOfMonth) =
+     weekday == otherWeekday && dayOfMonth == otherDayOfMonth
