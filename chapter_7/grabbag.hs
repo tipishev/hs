249ed1c701694b54e2d34b7a267cff67e41bcb33
Timofey Tipishev <tipishev@gmail.com>
2018-04-18 20:39:18 +0200

    complete more lambda exercises

---
+++ chapter_7/grabbag.hs
@@ -0,0 +1,19 @@
+module GrabBag where
+
+  -- all 4 are equivalent
+  mTha x y z = x * y * z
+  mThb x y = \z -> x * y * z
+  mThc x = \y -> \z -> x * y * z
+  mThd = \x -> \y -> \z -> x * y * z
+
+
+  addOneIfOdd n = case odd n of
+    True -> f n
+    False -> n
+    where f = \n -> n + 1
+
+  addFive x y = (if x > y then y else x) + 5
+  addFive' y = \x -> (if x > y then y else x) + 5
+  addFive'' = \y -> \x -> (if x > y then y else x) + 5
+
+
