e0cefb7f5cef6878d3c39c180d0b91dce1a69681
Timofey Tipishev <tipishev@gmail.com>
2018-04-19 17:25:14 +0200

    add penguins and another example of PatternMatchi

--- chapter_7/pattern_matching.hs
+++ chapter_7/pattern_matching.hs
@@ -3,3 +3,9 @@ module PatternMatching where
   isItTwo :: Integer -> Bool
   isItTwo 2 = True
   isItTwo _ = False
+
+  f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+  f x y = ((snd x, snd y), (fst x, fst y))
+
+  f' :: (a, b) -> (c, d) -> ((b, d), (a, c))
+  f' (a, b) (c, d) = ((b, d), (a, c))
---
+++ chapter_7/penguin.hs
@@ -0,0 +1,51 @@
+module Penguins where
+  data WherePenguinsLive =
+      Galapagos
+    | Antarctica
+    | Australia
+    | SouthAfrica
+    | SouthAmerica
+    deriving (Eq, Show)
+
+  data Penguin =
+    Peng WherePenguinsLive
+    deriving (Eq, Show)
+
+  isSouthAfrica :: WherePenguinsLive -> Bool
+  isSouthAfrica SouthAfrica = True
+  isSouthAfrica _ = False
+
+  gimmeWhereTheyLive :: Penguin -> WherePenguinsLive
+  gimmeWhereTheyLive (Peng whereItLives) = whereItLives
+
+  humboldt :: Penguin
+  humboldt = Peng SouthAmerica
+
+  gentoo :: Penguin
+  gentoo = Peng Antarctica
+
+  macaroni :: Penguin
+  macaroni = Peng Antarctica
+
+  little :: Penguin
+  little = Peng Australia
+
+  galapagos :: Penguin
+  galapagos = Peng Galapagos
+
+  galapagosPenguin :: Penguin -> Bool
+  galapagosPenguin (Peng Galapagos) = True
+  galapagosPenguin _ = False
+
+  antarcticPenguin :: Penguin -> Bool
+  antarcticPenguin (Peng Antarctica) = True
+  antarcticPenguin _ = False
+
+  -- in this final function, the || operator
+  -- -- is an `or` function, which will return True
+  -- -- if either value is True
+
+  antarcticOrGalapagos :: Penguin -> Bool
+  antarcticOrGalapagos p = (galapagosPenguin p) || (antarcticPenguin p)
+
+
