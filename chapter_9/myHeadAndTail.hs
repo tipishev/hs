b424be956d93667a81c3418720f5e4402d2339ac
Timofey Tipishev <tipishev@gmail.com>
2018-05-16 21:28:29 +0200

    add myHeadAndTail example

---
+++ chapter_9/myHeadAndTail.hs
@@ -0,0 +1,10 @@
+module MyHeadAndTail where
+  myTail :: [a] -> Maybe [a]
+  myTail [] = Nothing
+  myTail (x:[]) = Nothing
+  myTail (_:xs) = Just xs
+
+  myHead :: [a] -> Maybe a
+  myHead [] = Nothing
+  myHead (x:_) = Just x
+  
